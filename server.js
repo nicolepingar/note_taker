73b1f5f99d1a80781792997e13dc0783480da407
Nicole Pingar <nicolepingar@yahoo.com>
2021-10-07 15:53:43 -0400

    post and get done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
+const readFromFile = util.promisify(fs.readFile);
 
 const app = express();
 
@@ -12,17 +14,59 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+// html routes
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => {
-    res.json(`${req.method} request received to get reviews`);
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 }
 );
+// api routes
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a tip`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Tip added successfully`);
+    } else {
+        res.error('Error in adding tip');
+    }
+});
+
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} `)
+    console.log(`App listening at http://localhost:${PORT} `)
 );
 
